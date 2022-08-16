623587dd50f77e9fef4cd50a94e152b87a69c65e
Dani Carrascosa <danielcarrascosa2005@gmail.com>
2022-08-16 18:55:30 -0300

    commit angular fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'password-manager-dani',
+    appId: '1:324231805460:web:69e5f8c3ccae079d320501',
+    databaseURL: 'https://password-manager-dani-default-rtdb.firebaseio.com',
+    storageBucket: 'password-manager-dani.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDpXu-M1EYd9UWwabLjpwBq-DSG8HmklrM',
+    authDomain: 'password-manager-dani.firebaseapp.com',
+    messagingSenderId: '324231805460',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'password-manager-dani',
+    appId: '1:324231805460:web:69e5f8c3ccae079d320501',
+    databaseURL: 'https://password-manager-dani-default-rtdb.firebaseio.com',
+    storageBucket: 'password-manager-dani.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDpXu-M1EYd9UWwabLjpwBq-DSG8HmklrM',
+    authDomain: 'password-manager-dani.firebaseapp.com',
+    messagingSenderId: '324231805460',
+  },
   production: false
 };
 
